bd9595d77943f1925cde396bbb9084184667c853
Peter Gamali <petrugamali@gmail.com>
2022-08-19 23:13:08 +0300

    Added User and Token schemas

---
+++ schemas/Token.js
@@ -0,0 +1,8 @@
+import { Schema, model } from 'mongoose'
+
+const Token = new Schema({
+    user: { type: Schema.Types.ObjectId, ref: 'User' },
+    refreshToken: { type: String, required: true }
+})
+
+export default model('Token', Token);
