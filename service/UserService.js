5fad72ee3d4356ff6597f7d5d6d28e2314fb58d9
Peter Gamali <petrugamali@gmail.com>
2022-08-19 23:44:53 +0300

    Added registration method for user service

--- service/UserService.js
+++ service/UserService.js
@@ -1,12 +1,17 @@
 import User from "../schemas/User.js"
-
+import bcrypt from 'bcrypt'
+import { v4 as uuidv4 } from 'uuid'
+import mailService from './MailService.js'
 class UserService {
     async registration(email, password) {
         const candidate = await User.findOne({ email })
         if (candidate) {
             throw new Error('The user with this email already exists')
         }
-        const user = await User.create({ email, password })
+        const hashedPassword = await bcrypt.hash(password, 3)
+        const activationLink = uuid.v4()
+        const user = await User.create({ email, password: hashedPassword, activationLink })
+        await mailService.sendActivationMail(email, activationLink)
     }
 }
 
